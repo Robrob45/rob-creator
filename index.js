a8984c4b7aab94b6000a2d56a63897dbc7c6cd73
Robrob45 <160436517+Robrob45@users.noreply.github.com>
2024-09-13 11:22:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let menuBtn = document.getElementById('menuBtn')
-let sideNav = document.getElementById('sideNav')
-let menu = document.getElementById('menu')
+let menuBtn = document.getElementById('menuBtn');
+let sideNav = document.getElementById('sideNav');
+let menu = document.getElementById('menu');
 
-menu.onclick = function(){
-    if(sideNav.style.right=='-250px'){
-        sideNav.style.right='0';
-        menu.src = 'image/w.png';
+menu.onclick = function() {
+    if (sideNav.style.right === '-250px' || sideNav.style.right === '') {
+        sideNav.style.right = '0';
+        menu.src = './Image/w.png'; // Ensure the path is correct
+    } else {
+        sideNav.style.right = '-250px';
+        menu.src = './Image/icons8-menu-30.png'; // Ensure the path is correct
     }
-    else{
-        sideNav.style.right='-250px';
-        menu.src = 'Image/icons8-menu-30.png';
-    }
-}
+};
